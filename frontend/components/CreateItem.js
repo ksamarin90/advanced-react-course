50a7d03620042beea724a28eb466a17c7e93a20c
ksamarin90 <60395970+ksamarin90@users.noreply.github.com>
2020-09-22 12:34:07 +0300

    Client Side GraphQL
    1. Uploading images on Cloudinary
    2. Updating items
    3. Deleting items

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -39,6 +39,28 @@ class CreateItem extends Component {
     const val = type === 'number' ? parseFloat(value) : value;
     this.setState({ [name]: val });
   };
+
+  uploadFile = async (e) => {
+    console.log('Uploading file');
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+    console.log(data)
+
+    const res = await fetch('https://api.cloudinary.com/v1_1/dmeaixoor/image/upload', {
+      method: 'POST',
+      body: data
+    });
+
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    })
+  }
+
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -59,6 +81,19 @@ class CreateItem extends Component {
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && <img width="200" src={this.state.image} alt={this.state.title}/>
+                }
+              </label>
               <label htmlFor="title">
                 Title
                 <input
