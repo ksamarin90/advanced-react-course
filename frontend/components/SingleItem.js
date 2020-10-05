083263294d670720524c002b9d409c5500dd2866
ksamarin90 <60395970+ksamarin90@users.noreply.github.com>
2020-10-05 21:04:52 +0300

    Testing and Mocking Apollo Queries

--- frontend/components/SingleItem.js
+++ frontend/components/SingleItem.js
@@ -26,7 +26,7 @@ const SingleItemStyles = styled.div`
 
 const SINGLE_ITEM_QUERY = gql`
   query SINGLE_ITEM_QUERY($id: ID!) {
-    item(where: {id: $id}) {
+    item(where: { id: $id }) {
       id
       title
       description
@@ -34,30 +34,37 @@ const SINGLE_ITEM_QUERY = gql`
     }
   }
 `;
-
 class SingleItem extends Component {
   render() {
     return (
-      <Query query={SINGLE_ITEM_QUERY} variables={{id: this.props.id}}>
-        {({error, loading, data}) => {
-          if (error) return <Error error={error}/>;
-          if (loading) return <Error>Loading...</Error>;
-          if (!data.item) return <p>No Data Found for {this.props.id}</p>;
+      <Query
+        query={SINGLE_ITEM_QUERY}
+        variables={{
+          id: this.props.id,
+        }}
+      >
+        {({ error, loading, data }) => {
+          if (error) return <Error error={error} />;
+          if (loading) return <p>Loading...</p>;
+          if (!data.item) return <p>No Item Found for {this.props.id}</p>;
           const item = data.item;
-          return <SingleItemStyles>
-            <Head>
-              <title>Sick fits | {item.title}</title>
-            </Head>
-            <img src={item.largeImage} alt={item.title}/>
-            <div className="details">
-              <h2>Viewing {item.title}</h2>
-              <p>{item.description}</p>
-            </div>
-          </SingleItemStyles>;
+          return (
+            <SingleItemStyles>
+              <Head>
+                <title>Sick Fits | {item.title}</title>
+              </Head>
+              <img src={item.largeImage} alt={item.title} />
+              <div className="details">
+                <h2>Viewing {item.title}</h2>
+                <p>{item.description}</p>
+              </div>
+            </SingleItemStyles>
+          );
         }}
       </Query>
-    )
+    );
   }
 }
 
 export default SingleItem;
+export { SINGLE_ITEM_QUERY };
