48f9e23f766f7ee8f4407133f89693cca622d501
ksamarin90 <60395970+ksamarin90@users.noreply.github.com>
2020-10-08 21:40:19 +0300

    prod url

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
+export const endpointProd = `https://sickfits-yoga-samarin-prod.herokuapp.com/`;
 export const perPage = 4;
